12cd11cf9dbf5017ef113f69f91cb405985e6f94
Jose Gomez <j.gomez4@sapalomera.com>
2025-03-26 18:48:01 +0100

    add user profile customization + some small qol

--- projecteFinalDAW/user/forms.py
+++ projecteFinalDAW/user/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 class Config(forms.Form):
     
-    username = forms.CharField(label="Nom de l'usuari", max_length=100)
-    email = forms.EmailField(label="Correu electrònic")
+    username = forms.CharField(
+        label="Nom de l'usuari", 
+        max_length=100, 
+        widget=forms.TextInput(attrs={'class': 'configInputs'})
+    )
+    email = forms.EmailField(
+        label="Correu electrònic",
+        widget=forms.EmailInput(attrs={'class': 'configInputs'})
+    )
     
     
--- projecteFinalDAW/user/views.py
+++ projecteFinalDAW/user/views.py
@@ -13,10 +13,34 @@ def configuration(request):
                 "username": request.user.username,
                 "email": request.user.email
             }
+            
+            form = Config(initial=initial_data)  
+            return render(request, "userConfiguration.html", {"form": form})
         else:
-            initial_data = {}
-
-    form = Config(initial=initial_data)  
-    return render(request, "userConfiguration.html", {"form": form})
+            return render(request, "userConfiguration.html", {"form": form, "error": "Has de logar-te per poder accedir a aquesta pàgina"})
+    elif request.method == 'POST':
+        
+        initial_data = {}
+        username = request.POST['username']
+        email = request.POST['email']
+        
+        print(username)
+        print(email)
+        
+        print(request.user.username)
+        print(request.user.email)
+        
+        if username != request.user.username or email != request.user.email:
+            
+            user = request.user
+            user.username = username
+            user.email = email
+            user.save()
+            form = Config(initial={"username": user.username, "email": user.email})
+            return render(request, "userConfiguration.html", {"form": form, "message": "La configuració s'ha actualitzat correctament"})
+        else:
+            form = Config(initial={"username": username, "email": email})
+            return render(request, "userConfiguration.html", {"form": form, "error": "Has de canviar algun camp per poder actualitzar la configuració"})
+    
 
 
