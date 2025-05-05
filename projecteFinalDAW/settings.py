81206d10495199545644004d8bf289a6c622abe7
Jose Gomez <j.gomez4@sapalomera.com>
2025-05-05 09:27:46 +0200

    +fixing

--- projecteFinalDAW/settings.py
+++ projecteFinalDAW/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'projecteFinalDAW.urls'
@@ -126,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 LOGIN_URL = "/ login"
 
